3f25641deeeb378febf0f6b9afbcf5b78367c28e
Brayan Narvaez <131482664+BNarvaez@users.noreply.github.com>
2025-03-07 19:24:29 -0500

    Update productos.js

--- src/schemas/productos.js
+++ src/schemas/productos.js
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose';
 
-const productosSchema = new mongoose.Schema(
-    {
-        nombre: { type: String, required: true},
-        precio: { type: Number, required: true },   
-        descripcion: { type: String, required: true},
-        /* codigo: { type: String, required: true, max: 100 }, */
-        foto: { type: String, required: true},
-        stock: { type: Number, required: true },
-        precioPromo: { type: Number, required: true},
-        categoria: { type: String, required: true},
-    },
-    { timestamps: true, versionKey: false }
+const productsSchema = new mongoose.Schema(
+{
+    nombre: { type: String, required: true },
+    precio: { type: Number, required: true },
+    descripcion: { type: String, required: true },
+    /* codigo: { type: String, required: true, max: 100 }, */
+    foto: { type: String, required: true },
+    stock: { type: Number, required: true },
+    precioPromo: { type: Number, required: true },
+    categoria: { 
+        type: String, 
+        required: true, 
+        enum: ['medicamentos', 'belleza', 'suplementos'], 
+        default: 'medicamentos' 
+    }
+},
+{ timestamps: true, versionKey: false }
+);
 
-)
-
-export default mongoose.model('productos', productosSchema);
+export default mongoose.model('products', productsSchema);
