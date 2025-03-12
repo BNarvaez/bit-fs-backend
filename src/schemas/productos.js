83b24703b2eae0c5df12ec0f6b1f62440e522211
Brayan Narvaez <131482664+BNarvaez@users.noreply.github.com>
2025-03-11 20:50:33 -0500

    Update backend

--- src/schemas/productos.js
+++ src/schemas/productos.js
@@ -5,15 +5,13 @@ const productsSchema = new mongoose.Schema(
     nombre: { type: String, required: true },
     precio: { type: Number, required: true },
     descripcion: { type: String, required: true },
-    /* codigo: { type: String, required: true, max: 100 }, */
     foto: { type: String, required: true },
-    stock: { type: Number, required: true },
-    precioPromo: { type: Number, required: true },
+    cantidad: { type: Number, required: true },
     categoria: { 
         type: String, 
         required: true, 
-        enum: ['medicamentos', 'belleza', 'suplementos'], 
-        default: 'medicamentos' 
+        enum: ['Medicamentos', 'Belleza', 'Suplementos'], 
+        default: 'Medicamentos' 
     }
 },
 { timestamps: true, versionKey: false }
